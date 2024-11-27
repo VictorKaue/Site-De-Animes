41cad5c053ed507d45ec4647d5115ffed42a871f
VictorKaue <victorkaue909@gmail.com>
2024-11-27 01:38:22 -0300

    codigo final pronto

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,69 @@
-// Espera o carregamento do DOM antes de adicionar os ouvintes de evento
 document.addEventListener('DOMContentLoaded', function () {
-    // Adiciona o evento de clique no avatar do usuário
     document.getElementById('user-avatar').addEventListener('click', function(event) {
         const options = document.getElementById('user-options');
-        // Alterna a visibilidade do dropdown
         options.style.display = (options.style.display === 'block' ? 'none' : 'block');
-        event.stopPropagation(); // Impede que o clique no dropdown feche imediatamente
+        event.stopPropagation(); 
     });
 
-    // Fecha o dropdown se o usuário clicar fora dele
     document.addEventListener('click', function(event) {
         const options = document.getElementById('user-options');
-        // Verifica se o clique não foi nem no menu nem no avatar
         if (!options.contains(event.target) && !document.getElementById('user-avatar').contains(event.target)) {
             options.style.display = 'none';
         }
     });
+});
+
+$('#formCadastro').submit(function(event) {
+    event.preventDefault(); 
+
+    var senha = $('#senha').val();
+    var confirmaSenha = $('#confirmaSenha').val();
+
+    if (senha !== confirmaSenha) {
+        alert("As senhas não coincidem.");
+        return;
+    }
+
+    $('#cadastroModal').modal('hide');
+
+    window.location.href = 'home.html';
+});
+
+$('#formLogin').submit(function(event) {
+    event.preventDefault();
+
+    var email = $('#email').val();
+    var senha = $('#senha').val();
+
+    if (email && senha) {
+        $('#loginModal').modal('hide');
+        window.location.href = 'home.html';
+    } else {
+        alert("Por favor, preencha todos os campos.");
+    }
+});
+
+
+function confirmarSair() {
+    if (confirm("Você tem certeza que deseja sair?")) {
+        window.location.href = "index.html";
+    }
+}
+
+document.getElementById('formAlterarEmail').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert("E-mail alterado com sucesso!");
+});
+
+document.getElementById('formAlterarSenha').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const novaSenha = document.getElementById('novaSenha').value;
+    const confirmarSenha = document.getElementById('confirmarSenha').value;
+    
+    if (novaSenha !== confirmarSenha) {
+        alert("As senhas não coincidem!");
+    } else {
+        alert("Senha alterada com sucesso!");
+    }
 });
